fdae56ab9b9c892ec776b450b290c510f8da77c9
micah <micah@glowcoil.com>
2021-12-16 17:08:01 -0600

    event filter extension

---
+++ src/ext/event_filter.rs
@@ -0,0 +1,18 @@
+use crate::{events::*, host::*, plugin::*};
+
+use std::os::raw::c_char;
+
+pub const CLAP_EXT_EVENT_FILTER: *const c_char = b"clap.event-filter\0".as_ptr() as *const c_char;
+
+#[repr(C)]
+#[derive(Copy, Clone)]
+pub struct clap_plugin_event_filter {
+    pub accepts:
+        unsafe extern "C" fn(plugin: *const clap_plugin, event_type: clap_event_type) -> bool,
+}
+
+#[repr(C)]
+#[derive(Copy, Clone)]
+pub struct clap_host_event_filter {
+    pub changed: unsafe extern "C" fn(host: *const clap_host),
+}
--- src/ext/mod.rs
+++ src/ext/mod.rs
@@ -1,2 +1,3 @@
 pub mod audio_ports;
 pub mod audio_ports_config;
+pub mod event_filter;
