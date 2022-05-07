9d295d2e360145a527015ac93d2d8674d1254ed6
Robbert van der Helm <mail@robbertvanderhelm.nl>
2022-05-07 15:53:50 +0200

    Add the preset-load draft extension

--- src/ext/draft/mod.rs
+++ src/ext/draft/mod.rs
@@ -3,3 +3,4 @@ pub mod check_for_update;
 pub mod cv;
 pub mod file_reference;
 pub mod midi_mappings;
+pub mod preset_load;
---
+++ src/ext/draft/preset_load.rs
@@ -0,0 +1,12 @@
+use crate::plugin::*;
+
+use std::os::raw::c_char;
+
+pub const CLAP_EXT_PRESET_LOAD: *const c_char =
+    b"clap.preset-load.draft/0\0".as_ptr() as *const c_char;
+
+#[repr(C)]
+#[derive(Copy, Clone)]
+pub struct clap_plugin_preset_load {
+    pub from_file: unsafe extern "C" fn(plugin: *const clap_plugin, path: *const c_char) -> bool,
+}
